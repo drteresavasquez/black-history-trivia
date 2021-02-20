9ca0fbefed4ed7ab35d06d611454a019c01628dd
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-02-19 20:43:16 -0600

    updated user method

--- src/javascripts/helpers/auth.js
+++ src/javascripts/helpers/auth.js
@@ -11,7 +11,6 @@ const checkLoginStatus = () => {
     if (user) {
       // person is logged in do something...
       logoutButton();
-      console.warn(firebase.auth().currentUser.uid);
       userData(user); // checks for new user
     } else {
       // person is NOT logged in
