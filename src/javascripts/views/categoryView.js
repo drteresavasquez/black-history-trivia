6116fb1087c3778251ab4b1493fecddf1f569f78
curtis c <crutchc@yahoo.com>
2021-02-20 15:39:42 -0600

    Added Icon to browser tab

--- src/javascripts/views/categoryView.js
+++ src/javascripts/views/categoryView.js
@@ -2,9 +2,8 @@ import getCategories from '../helpers/data/categoryData';
 
 const categoryViewer = () => {
   getCategories().then((response) => response.forEach((element) => {
-    document.querySelector(
-      '#app'
-    ).innerHTML += `<button id="cat-btns" type="button" class="btn btn-outline-success">${element.name}</button>`;
+    document.querySelector('#app').innerHTML += `
+    <button id="cat-btns" type="button" class="btn btn-outline-success">${element.name}</button>`;
   }));
 };
 export default categoryViewer;
