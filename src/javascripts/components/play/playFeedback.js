510541ab4811ef16847fc711d3b855e9e3416a3e
Trinity Christiana <trinitycterry@gmail.com>
2021-02-20 21:28:59 -0600

    Update next question if last one

--- src/javascripts/components/play/playFeedback.js
+++ src/javascripts/components/play/playFeedback.js
@@ -1,15 +1,16 @@
 import { getCurrentAnswer, updateCurrentAnswer } from '../../helpers/data/currentQuizData';
 
 const playFeedback = () => {
-  const { question } = getCurrentAnswer();
+  const { question, index, allQuestions } = getCurrentAnswer();
   const { correctAnswer, selectedAnswer } = question;
   const gotCorrect = correctAnswer === selectedAnswer;
+  const isLast = index + 1 === allQuestions.length;
   updateCurrentAnswer({ gotCorrect });
   document.querySelector('#play-body').innerHTML = `
   <div class="play-feedback play-feedback-${gotCorrect ? 'correct' : 'incorrect'}">
     <div class="play-feedback-title">${gotCorrect ? 'You Answer is Correct' : 'You Answer is Incorrect'}</div>
     <div class="play-feedback-image"></div>
-    <div class="play-feedback-button-container"><button id="play-next-question" type="button" class="btn btn-light">Next Question</button></div>
+    <div class="play-feedback-button-container"><button id="play-next-question" type="button" class="btn">${isLast ? 'Finish Quiz' : 'Next Question'}</button></div>
   </div>
 `;
 };
